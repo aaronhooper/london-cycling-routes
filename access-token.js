ef2b42b6263e8a72e5b3e4dbae2e2d202864d6ec
Aaron Hooper <rnjckhpr@gmail.com>
2023-03-23 02:05:47 +0000

    Add access token

--- access-token.js
+++ access-token.js
@@ -1,1 +1,1 @@
-mapboxgl.accessToken = '' // eslint-disable-line no-undef
+mapboxgl.accessToken = 'pk.eyJ1IjoiYWFyb25ob29wZXIiLCJhIjoiY2xma2gxZDF3MGF2cDNzbXRlNjY0dDQ4MiJ9.TpgmOC19qKnf_Bi28BJuHw' // eslint-disable-line no-undef
